c73492aa188fc7e7a26a4be8fb1146b942c60977
PatrickZ <pzarkha@hotmail.com>
2019-02-13 13:32:05 -0600

    first commit

---
+++ server/public/client.js
@@ -0,0 +1,85 @@
+console.log('client.js is loaded');
+
+
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('jQuery is ready');
+                                //NEEDED A DESCENDENT SELECTOR
+    $('#restaurantBody').on('click', '.deleteButton', handleDeleteClick);
+    $('#submitButton').on('click', handleSubmitClick);
+
+    appendTable()
+}
+
+
+
+function handleSubmitClick() {
+    console.log('Submitting!');
+
+    $.ajax({
+        method: "POST",
+        url: "/new",
+        data: {
+            name: $('#restaurantNameIn').val(),
+            type: $('#restaurantTypeIn').val(),
+        }
+    }).then(function() {  
+        $.ajax({
+        type: 'GET',
+        url: '/restaurants'
+        })
+        .then(function (response) {
+            //$('#outputDiv').empty();
+                console.log(response);
+                
+                appendTable(response)
+            
+        }
+        )
+    })
+    
+}
+
+
+function handleDeleteClick() {
+    console.log('Deleteing!');
+    $.ajax({
+        method: "DELETE",
+        url: "/restaurants",
+        data: {index: $(this).data('index')}
+    }).then(function(response){
+        console.log('back form DELETE with:', response);
+        //update dom
+        appendTable();
+    }).catch( function( error ){
+        console.log('error with DELETE:', error);
+    })
+}
+
+
+function appendTable() {
+    $.ajax({
+
+        url: '/restaurants',
+        method: 'GET'
+
+    }).then(function(response) {
+        console.log('requesting restaurants');
+
+        console.log(response);
+        $('#restaurantBody').empty();
+        response.forEach(function(restaurant){   
+
+        $('#restaurantBody').append(
+        
+        `<tr>
+        <td>${restaurant.name}</td>
+        <td>${restaurant.type}</td> 
+        <td><button class="deleteButton" data-index="${restaurant.id}" >Delete</button></td>
+        </tr>`
+        )
+    });
+    })
+}
---
+++ server/server.js
@@ -0,0 +1,67 @@
+const express = require( 'express' );
+const app = express();
+const pg = require('pg');
+const bodyParser = require( 'body-parser' );
+const PORT = 5000;
+
+const pool = pg.Pool({
+    host: 'localhost',   //where is your database
+    port: 5432,          // what port is your database on
+    database: 'restuarants', //name of database
+    max: 10,                    // max # of connections
+    idleTimeoutMillis: 30000 
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// serve back static files
+app.use(express.static('server/public'));
+
+// spin up server
+app.listen(PORT, function(){
+  console.log('server running on: ', PORT);
+}); 
+
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+});
+
+pool.on('error', (error) => {
+    console.log('Error with postgres pool', error);
+    
+});
+
+app.get('/restaurants', (req, res) => {
+    pool.query('SELECT * FROM "restaurants"')
+    .then((response) => {
+        // console.log('response:', response.rows);
+       
+       res.send(response.rows);
+    }).catch((error) => {
+        console.log('error with restaurant select:', error);
+        res.sendStatus(500);
+    })
+});
+
+
+app.post('/new', (req, res) => {
+    console.log('/restuarants post route was hit');
+    pool.query(`INSERT INTO "restaurants" ("name", "type")
+    VALUES ( $1, $2 );`, [req.body.name, req.body.type])
+    .then(() => {
+        res.sendStatus(200)
+    }).catch((error) => {
+        console.log('error with restuarant insert:', error);
+        res.sendStatus(500)
+    })
+});
+
+app.delete('/restaurants', (req, res) => {
+    console.log('in /restaurants DELETE:', req.body);
+    pool.query(`DELETE FROM "restaurants" WHERE ("id" = ${req.body.index})`)
+    res.send('ribbet');
+
+
+})
+
